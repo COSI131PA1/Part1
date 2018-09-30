2a5d084e54cbdc677fc4537a0517696dc0798443
NinaXu317 <c1xu@brandeis.edu>
2018-09-29 21:53:17 -0400

    aaa

--- src/cs131/pa1/filter/sequential/CatFilter.java
+++ src/cs131/pa1/filter/sequential/CatFilter.java
@@ -3,48 +3,58 @@ package cs131.pa1.filter.sequential;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class CatFilter extends SequentialFilter {
 	private String command;
+	private ArrayList<String> filesRead =  new ArrayList<String>();
+	private int processedFile = 0;
+	
 	public CatFilter(String command) {
 		this.command = command;
 	}
 	
-	public void output(ArrayList<String> filesRead) {
-		for (String fileName: filesRead) {
-			try { 
-				File f = new File(fileName);
-				Scanner console = new Scanner(f);
-				if (console.hasNextLine()) {
-					String line = console.nextLine();
-					String processLine = processLine(line);
-					if (processLine != null ) {
-						this.output.add(processLine);
-					}
-				}
-			} catch (FileNotFoundException e) {
-			    System.out.println(e.getMessage());
-			}
-		}
-	}
-	
-	public void process() {
+	public void getFiles() {
 		ArrayList<String> filesRead =  new ArrayList<String>();
 		String[] inputArray = command.split("\\s+");
 		for (int i = 0; i <  inputArray.length; i++) {
 			if (!inputArray[i].equals("cat")) {
-				filesRead.add(inputArray[i]);
+				this.filesRead.add(inputArray[i]);
 			}
 		}
-		output(filesRead);
-	}
 
+	}
+	
+	
+	public void process() {
+		getFiles();
+//		output.add("aaaa");
+//		System.out.println("!!!! " + output);
+//		System.out.println(this.output.isEmpty());
+//		System.out.print("new output: "+ output.size());
+		String currentDir = SequentialREPL.currentWorkingDirectory;
+		for (String fileName: this.filesRead) {
+			File f = new File(fileName);
+			Scanner console;
+			try {
+				console = new Scanner (f);
+				while (console.hasNextLine()) {
+					this.output.add(console.nextLine());
+//					System.out.println(console.nextLine());
+				}
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+			processedFile++;
+		}
+	}
+	public boolean isDone() {
+		return processedFile >= filesRead.size();
+	}
+	
 	@Override
 	protected String processLine(String line) {
-		if (line != null) {
-			return line;
-		} 
 		return null;
 	}
 }
---
+++ src/cs131/pa1/filter/sequential/test1.java
@@ -0,0 +1,11 @@
+package cs131.pa1.filter.sequential;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		CatFilter cat = new CatFilter("cat hello-world.txt");
+		cat.process();
+		
+	}
+
+}
